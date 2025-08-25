1f5b9d9e588c57bc64d54ef6f7a17da33d862fd3
Sam Ilmetov <cepuhin.semen@gmail.com>
2025-08-26 01:06:02 +1000

    Задание №2: Пагинация для отелей

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+from pydantic import Field
 from fastapi import FastAPI, Query, Body, HTTPException
-from fastapi.openapi.docs import get_swagger_ui_html
 import uvicorn
 from fastapi.routing import APIRoute
 from pydantic import BaseModel
@@ -8,24 +8,31 @@ from pydantic import BaseModel
 app = FastAPI()
 
 hotels = [
-    {"id": 1, "city": "Воронеж", "country":"Россия"},
-    {"id": 2, "city": "Сочи","country":"Россия"},
-    {"id": 3 ,"city": "Париж","country":"Франция"},
+    {"id": 1, "title": "Sochi", "name": "sochi"},
+    {"id": 2, "title": "Дубай", "name": "dubai"},
+    {"id": 3, "title": "Мальдивы", "name": "maldivi"},
+    {"id": 4, "title": "Геленджик", "name": "gelendzhik"},
+    {"id": 5, "title": "Москва", "name": "moscow"},
+    {"id": 6, "title": "Казань", "name": "kazan"},
+    {"id": 7, "title": "Санкт-Петербург", "name": "spb"},
 ]
+
 
 @app.get("/hotels")
 def get_hotels(
         id : int | None = Query(default=None,description="Айди"),
-        city : str | None = Query(default=None,description="Название отеля"),
+        title : str | None = Query(default=None,description="Название отеля"),
+        page:int | None = 1,
+        per_page:int | None = 3,
 ):
     hotels_ = []
     for hotel in hotels:
         if id and hotel["id"] != id:
             continue
-        if city and hotel["city"] != city:
+        if title and hotel["title"] != title:
             continue
         hotels_.append(hotel)
-    return hotels_
+    return hotels_[(page-1) * per_page : (page-1) * per_page + per_page]
 
 @app.delete("/hotels/{hotel_id}")
 def delete_hotels(
@@ -37,15 +44,15 @@ def delete_hotels(
 
 @app.post("/hotels")
 def create_hotel(
-    city: str = Body(embed=True),
-    country: str = Body(embed=True),
+    title: str = Body(embed=True),
+    name: str = Body(embed=True),
 ):
     global hotels
     hotels.append(
         {
             "id": hotels[-1]["id"]+1,
-            "city":city,
-            "country":country,
+            "title":title,
+            "name":name,
         }
     )
     return {"status": 200, "hotels": hotels}
@@ -53,12 +60,12 @@ def create_hotel(
 
 # Модель для частичного обновления
 class HotelUpdate(BaseModel):
-    city: str | None = None
-    country: str | None = None
+    title: str | None = Field(None)
+    name: str | None = Field(None)
 
 class HotelReplace(BaseModel):
-    city: str
-    country: str
+    title: str
+    name: str
 
 @app.patch("/hotels/{hotel_id}")
 def change_hotel(
@@ -83,6 +90,28 @@ def change_hotel(
 
     return {"status": 200, "hotels": hotels}
 
+@app.patch("/hotels/{hotel_id}")
+def change_hotel(
+    hotel_id: int,
+    update_data : HotelUpdate,
+):
+    hotel_to_update = None
+    for i,val in enumerate(hotels):
+        if hotels[i]["id"] == hotel_id:
+            hotel_to_update = hotels[i]
+            break
+
+    if not hotel_to_update:
+        raise HTTPException(status_code=404, detail="Отель не найден")
+
+    update_dict = update_data.model_dump(exclude_unset=True)
+    print(f"update_dict: {update_dict}")
+
+
+    for field, value in update_dict.items():
+        hotel_to_update[field] = value
+
+    return {"status": 200, "hotels": hotels}
 
 @app.put("/hotels/{hotel_id}")
 def change_hotel(
@@ -107,21 +136,24 @@ def change_hotel(
     return {"status": 200, "hotels": hotels}
 
 
+import time,asyncio
+
+@app.get("/sync/{id}")
+def sync_func(id:int):
+    print(f"sync начал {id}:{time.time():.2f}")
+    time.sleep(3)
+    print(f"sync закончил {id}:{time.time():.2f}")
+
+
+@app.get("/async/{id}")
+async def async_func(id:int):
+    print(f"async начал {id}:{time.time():.2f}")
+    await asyncio.sleep(3)
+    print(f"async закончил {id}:{time.time():.2f}")
 
 
 
-
-
-# @app.get("/docs", include_in_schema=False)
-# async def custom_swagger_ui_html():
-#     return get_swagger_ui_html(
-#         openapi_url=app.openapi_url,
-#         city=app.city + " - Swagger UI",
-#         oauth2_redirect_url=app.swagger_ui_oauth2_redirect_url,
-#         swagger_js_url="https://unpkg.com/swagger-ui-dist@5/swagger-ui-bundle.js",
-#         swagger_css_url="https://unpkg.com/swagger-ui-dist@5/swagger-ui.css",
-#     )
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app")
+    uvicorn.run("main:app",port=8000)
